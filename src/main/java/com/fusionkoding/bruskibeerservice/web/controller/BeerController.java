aca4a07767d55330de8cba4bb4a0addbfd1ba76c
Kyle Walter <walter.kl26@gmail.com>
2020-10-24 12:15:31 -0500

    Adding controller test

--- src/main/java/com/fusionkoding/bruskibeerservice/web/controller/BeerController.java
+++ src/main/java/com/fusionkoding/bruskibeerservice/web/controller/BeerController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/api/v1/been")
+@RequestMapping("/api/v1/beer")
 @RestController
 public class BeerController {
 
