881e0bf9540277167e75853f367ab15ffaca1bc8
Kyle Walter <walter.kl26@gmail.com>
2020-10-24 10:24:23 -0500

    Initial project created closes #1

---
+++ src/main/java/com/fusionkoding/bruskibeerservice/BruskiBeerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fusionkoding.bruskibeerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BruskiBeerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BruskiBeerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fusionkoding/bruskibeerservice/BruskiBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fusionkoding.bruskibeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BruskiBeerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
