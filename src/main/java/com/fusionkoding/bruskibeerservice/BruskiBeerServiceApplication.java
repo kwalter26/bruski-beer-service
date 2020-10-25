f38e093012eb84cd39972a72a5ebcd592feaf3e8
Kyle Walter <walter.kl26@gmail.com>
2020-10-24 21:45:08 -0500

    Removed commented code

--- src/main/java/com/fusionkoding/bruskibeerservice/BruskiBeerServiceApplication.java
+++ src/main/java/com/fusionkoding/bruskibeerservice/BruskiBeerServiceApplication.java
@@ -2,10 +2,8 @@ package com.fusionkoding.bruskibeerservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-// @EnableJpaRepositories
 public class BruskiBeerServiceApplication {
 
 	public static void main(String[] args) {
