7e02ae68b16ec21bbed008f2341dd163e0ead701
Kyle Walter <walter.kl26@gmail.com>
2021-06-05 20:10:21 -0500

    Fixing mapstruct

--- src/main/java/com/fusionkoding/bruskibeerservice/web/mappers/BeerMapper.java
+++ src/main/java/com/fusionkoding/bruskibeerservice/web/mappers/BeerMapper.java
@@ -3,10 +3,13 @@ package com.fusionkoding.bruskibeerservice.web.mappers;
 import com.fusionkoding.bruskibeerservice.domain.Beer;
 import com.fusionkoding.bruskibeerservice.web.model.BeerDto;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
-@Mapper(uses = {DateMapper.class}, componentModel = "spring")
+@Mapper(uses = {DateMapper.class})
 public interface BeerMapper {
+    @Mapping(source = "beerName", target = "beerName")
     BeerDto beerToBeerDto(Beer beer);
 
+    @Mapping(source = "beerName", target = "beerName")
     Beer beerDtoToBeer(BeerDto beerDto);
 }
