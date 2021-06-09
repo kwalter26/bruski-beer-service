037b9d5c2c90c456c7c284d19df8824c808d43ba
Kyle Walter <walter.kl26@gmail.com>
2021-06-08 21:01:32 -0500

    Update database settings

--- src/main/java/com/fusionkoding/bruskibeerservice/bootstrap/BeerLoader.java
+++ src/main/java/com/fusionkoding/bruskibeerservice/bootstrap/BeerLoader.java
@@ -2,10 +2,15 @@ package com.fusionkoding.bruskibeerservice.bootstrap;
 
 import com.fusionkoding.bruskibeerservice.domain.Beer;
 import com.fusionkoding.bruskibeerservice.repositories.BeerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Required;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 
+@RequiredArgsConstructor
+@Component
 public class BeerLoader implements CommandLineRunner {
 
     public static final String BEER_1_UPC = "0631234200036";
@@ -16,25 +21,43 @@ public class BeerLoader implements CommandLineRunner {
 
     private final BeerRepository beerRepository;
 
-    public BeerLoader(BeerRepository beerRepository) {
-        this.beerRepository = beerRepository;
-    }
-
     @Override
     public void run(String... args) {
-        loadBeerObjects();
+        if(beerRepository.count() == 0){
+            loadBeerObjects();
+        }
     }
 
     private void loadBeerObjects() {
 
-        if (beerRepository.count() == 0) {
-            beerRepository.save(Beer.builder().beerName("Guiness").beerStyle(BEER_STYLE_1).quantityToBrew(200)
-                    .minOnHand(12).upc(BEER_1_UPC).price(new BigDecimal("12.95")).build());
-            beerRepository.save(Beer.builder().beerName("Dragon's Milk").beerStyle(BEER_STYLE_1).quantityToBrew(200)
-                    .minOnHand(12).upc(BEER_2_UPC).price(new BigDecimal("12.95")).build());
-            beerRepository.save(Beer.builder().beerName("Gooma").beerStyle(BEER_STYLE_1).quantityToBrew(200)
-                    .minOnHand(12).upc(BEER_3_UPC).price(new BigDecimal("11.95")).build());
-        }
+
+            beerRepository.save(Beer.builder()
+                    .beerName("Guiness")
+                    .beerStyle(BEER_STYLE_1)
+                    .quantityToBrew(200)
+                    .minOnHand(12)
+                    .upc(BEER_1_UPC)
+                    .price(new BigDecimal("12.95"))
+                    .build());
+
+            beerRepository.save(Beer.builder()
+                    .beerName("Dragon's Milk")
+                    .beerStyle(BEER_STYLE_1)
+                    .quantityToBrew(200)
+                    .minOnHand(12)
+                    .upc(BEER_2_UPC)
+                    .price(new BigDecimal("12.95"))
+                    .build());
+
+            beerRepository.save(Beer.builder()
+                    .beerName("Gooma")
+                    .beerStyle(BEER_STYLE_1)
+                    .quantityToBrew(200)
+                    .minOnHand(12)
+                    .upc(BEER_3_UPC)
+                    .price(new BigDecimal("11.95"))
+                    .build());
+
     }
 
 }
