285e487d529636873e0939afad57a1c31a549436
Kyle Walter <walter.kl26@gmail.com>
2021-06-09 22:14:44 -0500

    Adding jms

---
+++ src/main/java/com/fusionkoding/bruskibeerservice/config/JmsConfig.java
@@ -0,0 +1,23 @@
+package com.fusionkoding.bruskibeerservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+@Configuration
+public class JmsConfig {
+
+    public static final String MY_QUEUE = "my-hello-world";
+    public static final String MY_SEND_AND_RECEIVE_QUEUE = "my-send-receive";
+
+    @Bean
+    public MessageConverter messageConverter() {
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+        return converter;
+    }
+
+}
